d6f29841d87c4bd9fbd23209e8040f9cf179adee
chokladio <chokladio@gmail.com>
2019-03-05 23:46:13 +0100

    ng generate component dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipesComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
