21b4615fac5c22494fdf7ddb544aa80ee35f29d6
Gustav Handmark <29983444+GustavHandmark@users.noreply.github.com>
2019-03-08 16:13:08 +0100

    made sure it compiles

--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -1,12 +1,8 @@
-import { getDiffieHellman } from "crypto";
-
 export class Recipe {
-
   recipe_id: number;
   title: string;
   recipe_url: string;
   directions: string;
-  portions: number;
   ingredients:(string | number)[]
   
   constructor(data: Object|Recipe) {
