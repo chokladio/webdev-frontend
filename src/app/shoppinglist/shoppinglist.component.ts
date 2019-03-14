098f9609b32d59b74ad0809f38589b62a568996b
Gustav Handmark <29983444+GustavHandmark@users.noreply.github.com>
2019-03-14 14:25:44 +0100

    Fixed data processing in shopping list, and fixed bugs in weekview and recipe pictures

--- src/app/shoppinglist/shoppinglist.component.ts
+++ src/app/shoppinglist/shoppinglist.component.ts
@@ -10,16 +10,15 @@ import { Recipe } from '../recipe';
 export class ShoppinglistComponent implements OnInit {
   private recipes: Recipe[];
   private allIngredients = [];
-  private clearedIngredients = [];
+  private clearedIngredients;
 
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getStoredRecipes()
-    //this.ingredients = [].concat.apply([],this.ingredients);
     console.log(this.recipes);
-    this.sortData();
+    this.processData();
   }
 
   apiCreateTest() {
@@ -29,8 +28,10 @@ export class ShoppinglistComponent implements OnInit {
       );
     }
   }
-  //For creating a cleaner shoppinglist, not yet finished.
-  sortData() {
+  /*Processing the ingredients data to separate units and amount and clean up data. Not perfect. We should've done this in the backend, when scraping recipes, but i wanted to include it 
+    here to see how the data can be processed.
+  */
+  processData() {
     const allIngredients = this.recipes.map(recipe => {
       return recipe.ingredients
     })
@@ -41,13 +42,95 @@ export class ShoppinglistComponent implements OnInit {
       return str.replace(/ *\([^)]*\) */g, " ").trim()
     })
 
-    const units = ['cup','teaspoon', 'tablespoons', 'tablespoon', 'pound', 'dash', 'cups', 'can'];
-    var regx = new RegExp(units.join("|"),"");
+    const replaceUnits = ['sprigs', 'sprig', 'cups', 'teaspoons', 'teaspoon', 'tablespoons', 'tablespoon', 'pounds', 'pound', 'dash', 'cans', 'can', 'cup', 'pinch', 'slices', 'ounces', 'ounce', 'packages', 'package', 'cloves', 'clove'];
+    const units = [];
+    var regx = new RegExp(replaceUnits.join("|"), "");
     copy.forEach((ingredient, index) => {
-      copy[index] = ingredient.replace(regx,'');
+      if (regx.test(ingredient)) {
+        copy[index] = ingredient.replace(regx, function (match) {
+          units.push(match);
+          return '';
+        }).trim();
+      } else {
+        units.push('');
+      }
     })
-    this.clearedIngredients = copy;
+    const amount = [];
+    //I love this dataset.
+    //Match any digit, one to unlimited times, optionally one forward slash, until first whitespace.
+    //However, after whitespace, if the serie continues, match digits, optional forward slash, until next whitespace.
+    //The ingredients may look like: 1 1/2 ounces. 3/4 onion
+    var regx2 = new RegExp('^\\d*[\\/]*?[\\s]*\\d+[\\/]*[^\\s]?')
+    copy.forEach((ingredient, index) => {
+      if (regx2.test(ingredient)) {
+        copy[index] = ingredient.replace(regx2, function (match) {
+          amount.push(match);
+          return '';
+        }).trim()
+      } else {
+        amount.push('');
+      }
+    })
+    //Purge non-essential descriptory words.
+    //I.e. chopped onion == onion however, fresh parsley != dried parsley.
+    //Why do they constantly give instructions in the ingredient listing?
+    let nones = ['coarsely', 'removed', 'if', 'divided', 'chunks', 'small', 'medium', 'thinly', 'seeded', 'cover', 'enough', 'quartered', 'topping', 'deveined', 'peeled', 'softened', 'beaten', 'taste', 'and', 'more', 'as', 'needed', 'halved', 'cut', 'into', 'cubes', 'taste', 'to', 'for', 'or', 'drained', 'finely', 'rinsed', 'chopped', 'shredded', 'softened', 'large', 'small', 'sliced', 'diced', 'crushed', 'cubed', 'cored', 'minced', 'melted']
+    //make sure we don't randomly delete characters in other words.
+    nones = nones.map(word => {
+      return `\\b${word}\\b`
+    })
+    var regx3 = new RegExp(nones.join("|"), "g");
+    copy.forEach((ingredient, index) => {
+      copy[index] = ingredient.replace(regx3, '');
+    })
+    //remove commas.
+    copy.forEach((ingredient, index) => {
+      copy[index] = ingredient.replace(/,|-/g, '').trim();
+    })
+    //And extra white spaces.
+    copy.forEach((ingredient, index) => {
+      copy[index] = ingredient.replace(/\s\s+/, ' ');
+    })
 
+    var imap = new Map();
+    /*
+    {
+      name:[unit,amount]
+    }
+    */
+    console.log(amount);
+    console.log(units);
+    console.log(copy);
+
+    //Eval would work in this case, but eval is unsafe and should never be used in this scenario (script injection attacks)
+    for (let i = 0; i < copy.length; i++) {
+      if (imap.get(copy[i]) && Number(imap.get(copy[i])[0]) !== NaN && Number(amount[i]) !== NaN) {
+        imap.set(copy[i], [+imap.get(copy[i])[0] + +(this.fracToDec(amount[i])),units[i]])
+      }
+      else {
+        imap.set(copy[i], [this.fracToDec(amount[i]),units[i]]);
+      }
+    }
+
+    this.clearedIngredients = imap;
+  }
+
+  fracToDec(s) {
+    console.log(s);
+    var y = s.split(' ');
+    if (y.length > 1) {
+      var z = y[1].split('/');
+      return (+y[0] + (+z[0] / +z[1]));
+    }
+    else {
+      var z = y[0].split('/');
+      if (z.length > 1) {
+        return (+z[0] / +z[1]);
+      }
+      else {
+        return (+z[0]);
+      }
+    }
   }
 
 }
